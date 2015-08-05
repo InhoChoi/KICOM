ddb0a7b52d845a242f1de07961611f33420d8d00
InhoChoi <revaer@naver.com>
2015-08-05 20:38:42 +0900

    I2C Display - Serial 통신으로 보내문자 바로 Display 시키게 작동

--- Arduino/display/display.ino
+++ Arduino/display/display.ino
@@ -5,15 +5,51 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x20 for a 16 chars and 2 line display
 
+String inputString = "";
+boolean stringComplete = false;
 void setup()
 {
+  Serial.begin(9600);
+  
   lcd.init();                      // initialize the lcd 
  
   // Print a message to the LCD.
   lcd.backlight();
-  lcd.print("Hello, world!");
+  lcd.print("Hello, World");
 }
 
 void loop()
 {
+  if(stringComplete){
+    int len = inputString.length();
+    
+    Serial.println(inputString);
+    if(len <= 16){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(inputString);
+    }else if( len > 16 && len<= 32){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(inputString);
+      inputString.remove(0,16);
+      lcd.setCursor(0,1);
+      lcd.print(inputString);
+    }
+    
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void serialEvent()
+{
+  while(Serial.available()){
+    char inChar = (char) Serial.read();
+    if(inChar == '\n'){
+      stringComplete = true;
+      break;
+    }
+    inputString += inChar;
+  }
 }
