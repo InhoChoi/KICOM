b39b4dc732bf8c7009910c4a9c0d9534649e3827
Sang Hyeon Kim <rlatkdgus500@gmail.com>
2015-08-07 03:36:23 +0900

    종료 버튼 구현 및 버튼 UI Setting

--- kicom/ImageButton.xaml.cs
+++ kicom/ImageButton.xaml.cs
@@ -13,78 +13,73 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace kicom
-{
+namespace kicom {
     /// <summary>
     /// ImageButton.xaml에 대한 상호 작용 논리
     /// </summary>
-public partial class ImageButton
-{
-	public ImageSource mImgOver;
-	public ImageSource ImgOver {
-		get { return mImgOver; }
-		set {
-			mImgOver = value;
-			Update();
-		}
-	}
+    public partial class ImageButton {
+        public ImageSource mImgOver;
+        public ImageSource ImgOver {
+            get { return mImgOver; }
+            set {
+                mImgOver = value;
+                Update();
+            }
+        }
 
-	public ImageSource mImgDown;
-	public ImageSource ImgDown {
-		get { return mImgDown; }
-		set {
-			mImgDown = value;
-			Update();
-		}
-	}
+        public ImageSource mImgDown;
+        public ImageSource ImgDown {
+            get { return mImgDown; }
+            set {
+                mImgDown = value;
+                Update();
+            }
+        }
 
-	public ImageSource mImgNormal;
-	public ImageSource imgNormal {
-		get { return mImgNormal; }
-		set {
-			mImgNormal = value;
-			Update();
-		}
-	}
+        public ImageSource mImgNormal;
+        public ImageSource imgNormal {
+            get { return mImgNormal; }
+            set {
+                mImgNormal = value;
+                Update();
+            }
+        }
 
-	public ImageSource mImgDisabled;
-	public ImageSource ImgDisabled {
-		get { return mImgDisabled; }
-		set {
-			mImgDisabled = value;
-			Update();
-		}
-	}
+        public ImageSource mImgDisabled;
+        public ImageSource ImgDisabled {
+            get { return mImgDisabled; }
+            set {
+                mImgDisabled = value;
+                Update();
+            }
+        }
 
-	private void ImageButton_Loaded(object sender, RoutedEventArgs e)
-	{
-		Update();
-	}
+        private void ImageButton_Loaded(object sender, RoutedEventArgs e) {
+            Update();
+        }
 
-	private void ImageButton_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
-	{
-		SetImgEnabled();
-	}
+        private void ImageButton_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e) {
+            SetImgEnabled();
+        }
 
-	private void Update()
-	{
-		this.Resources["imgOver"] = ImgOver;
-		this.Resources["imgDown"] = ImgDown;
-		SetImgEnabled();
-	}
+        private void Update() {
+            this.Resources["imgOver"] = ImgOver;
+            this.Resources["imgDown"] = ImgDown;
+            SetImgEnabled();
+        }
 
-	private void SetImgEnabled()
-	{
-		if (IsEnabled) {
-			this.Background = new ImageBrush(imgNormal);
-		} else {
-			this.Background = new ImageBrush(ImgDisabled);
-		}
-	}
-	public ImageButton()
-	{
-		IsEnabledChanged += ImageButton_IsEnabledChanged;
-		Loaded += ImageButton_Loaded;
-	}
-}
+        private void SetImgEnabled() {
+            if (IsEnabled) {
+                this.Background = new ImageBrush(imgNormal);
+            }
+            else {
+                this.Background = new ImageBrush(ImgDisabled); 
+            }
+        }
+
+        public ImageButton() {
+            IsEnabledChanged += ImageButton_IsEnabledChanged;
+            Loaded += ImageButton_Loaded;
+        }
+    }
 }
