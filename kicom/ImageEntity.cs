cf3e93195957b233a009548fa048c88dc9cbe37e
Sang Hyeon Kim <rlatkdgus500@gmail.com>
2015-08-07 23:27:07 +0900

    이미지 갤러리를 위한 클래스 추가
    ImageEntity와 ImageView

---
+++ kicom/ImageEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kicom {
+    public class ImageEntity {
+        #region Property
+
+      
+        public String ImagePath {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
---
+++ kicom/ImageView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace kicom {
+    public static class ImageView {
+        #region Methods
+
+        public static List<ImageEntity> GetAllImagesData() {
+            try {
+                // Load Xml Document
+                XDocument XDoc = XDocument.Load("ImageData.xml");
+
+                // Query for retriving all Images data from XML
+                var Query = from Q in XDoc.Descendants("Image")
+                            select new ImageEntity {
+                                ImagePath = Q.Element("ImagePath").Value
+                            };
+
+                // return images data
+                return Query.ToList<ImageEntity>();
+
+            }
+            catch (Exception ex) {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+    }
+}
