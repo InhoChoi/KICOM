1a8347da74cd23e08eec83b23fbfb456e81acbea
Sang Hyeon Kim <rlatkdgus500@gmail.com>
2015-08-08 03:31:58 +0900

    ImageList XML 형식 정리

--- kicom/WriteImageList.cs
+++ kicom/WriteImageList.cs
@@ -12,15 +12,15 @@ namespace kicom {
         private static Queue<string> ListingQueue = new Queue<string>();
 
         private WriteImageList() {
-            FileInfo _fileinfo = new FileInfo("Imagedata.xml");
+            FileInfo _fileinfo = new FileInfo("ImageData.xml");
 
             if (!_fileinfo.Exists) {
-                using (XmlTextWriter writer = new XmlTextWriter("Imagedata.xml", System.Text.Encoding.UTF8)) {
+                using (XmlTextWriter writer = new XmlTextWriter("ImageData.xml", System.Text.Encoding.UTF8)) {
                     writer.WriteStartDocument(true);
                     writer.Formatting = Formatting.Indented;
                     //writer.Indentation = 2;
-                    writer.WriteStartElement("Image");
                     writer.WriteStartElement("Images");
+                    writer.WriteStartElement("Image");
                     writer.WriteEndElement();
                     writer.WriteEndElement();
                     writer.Close();
@@ -58,15 +58,15 @@ namespace kicom {
                     XmlNode newNode;
 
                     xmlDoc = new XmlDocument();
-                    xmlDoc.Load("Imagedata.xml"); // XML문서 로딩
+                    xmlDoc.Load("ImageData.xml"); // XML문서 로딩
 
-                    newNode = xmlDoc.SelectSingleNode("Image/Images"); // 추가할 부모 Node 찾기
+                    newNode = xmlDoc.SelectSingleNode("Images/Image"); // 추가할 부모 Node 찾기
 
                     xmlEle = xmlDoc.CreateElement("ImagePath"); // 추가할 Node 생성
 
                     newNode.AppendChild(xmlEle); // 위에서 찾은 부모 Node에 자식 노드로 추가..
 
-                    xmlDoc.Save("Imagedata.xml"); // XML문서 저장..
+                    xmlDoc.Save("ImageData.xml"); // XML문서 저장..
                     xmlDoc = null;
 
                     return true;
