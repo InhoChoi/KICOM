3e0e6f9b0705e09a1306350ee91a3a10f8be7c74
JiyoungPark <realplay23@naver.com>
2015-08-07 15:48:39 +0900

    가독성을 위한 코드 일부 수정(kicom_list 관련 파일)

--- Web/static/javascript/kicom_list_js.js
+++ Web/static/javascript/kicom_list_js.js
@@ -7,24 +7,22 @@ $(document).ready(function(){
     success: function(xml){
       var xmlData = $(xml).find("Info");
       var listLength = xmlData.length;
+      var fTag = '<a href="';
+      var bTag = '">사진보기</a><br>';
+      var names = "";
+      var relations = "";
+      var dates = "";
+      var photos = "";
 
       if (listLength) {
-        var fTag = '<a href="';
-        var bTag = '">사진보기</a><br>';
-        var names = "";
-        var relations = "";
-        var dates = "";
-        var photos = "";
         var location = $(this).find("Photo_Path").text();
-
+        
         $(xmlData).each(function(){
-
           names += $(this).find("Name").text() + "<br>";
           relations += $(this).find("Relation").text() + "<br>";
           dates += $(this).find("Date").text() + "<br>"; 
           photos += fTag + $(this).find("Photo_Path").text() + bTag;
-
-          });
+        });
 
         $("#name_content").append(names);
         $("#relation_content").append(relations);
