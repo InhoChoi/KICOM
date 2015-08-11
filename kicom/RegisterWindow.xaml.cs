a6b0c5b8b42bdc4efe86b25e47045535d028ab4e
InhoChoi <revaer@naver.com>
2015-08-11 20:40:58 +0900

    New Windows Register

--- kicom/RegisterWindow.xaml.cs
+++ kicom/RegisterWindow.xaml.cs
@@ -18,8 +18,65 @@ namespace kicom {
     /// </summary>
     public partial class RegisterWindow : Window {
         public FaceAnalysis faceAnyalysis = null;
+        private string filePath = null;
+
         public RegisterWindow() {
             InitializeComponent();
         }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e) {
+            var openDlg = new Microsoft.Win32.OpenFileDialog();
+
+            openDlg.Filter = "JPEG Image(*.jpg)|*.jpg";
+            bool? result = openDlg.ShowDialog(this);
+
+            if (!(bool)result) {
+                return;
+            }
+
+            filePath = openDlg.FileName;
+
+            Uri fileUri = new Uri(filePath);
+            BitmapImage bitmapSource = new BitmapImage();
+
+            bitmapSource.BeginInit();
+            bitmapSource.CacheOption = BitmapCacheOption.None;
+            bitmapSource.UriSource = fileUri;
+            bitmapSource.EndInit();
+
+            FacePhoto.Source = bitmapSource;
+        }
+
+
+        private void Cancel_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private async void Register_Click(object sender, RoutedEventArgs e) {
+            if (faceAnyalysis == null) {
+                Console.Write("null!");
+                return;
+            }
+
+            if (this.Name_tb.Text == "" || this.Relation_tb.Text == "") {
+                MessageBox.Show("Must to fill the text block name and relation", "Error");
+                return;
+            }
+
+            int result = 0;
+            try {
+               result = await faceAnyalysis.register(Name_tb.Text, this.filePath, Relation_tb.Text, ETC_tb.Text);
+            }
+            catch(Exception err) {
+                MessageBox.Show(err.Message, "Error");
+                return;
+            }
+
+
+            MessageBox.Show("Face is registered", "Success!");
+            this.Close();
+
+        }
+
     }
 }
